2301c40a1c2fa08e7bfc81e458bc727d94adc02c
stephanied117 <145690972+stephanied117@users.noreply.github.com>
2025-04-24 12:53:22 -0400

    changed student num

--- Students.java
+++ Students.java
@@ -3,7 +3,7 @@ public class Students {
     public static int primaryKey;
 
     public static void generateStudents() {
-        for(int x = 1; x<2001; x++ ){
+        for(int x = 1; x<5001; x++ ){
             System.out.println("INSERT INTO Students (student_id, student_name) VALUES ( " + x + "," + " '" + "Student" + x + "' );");
             primaryKey = x;
         }
